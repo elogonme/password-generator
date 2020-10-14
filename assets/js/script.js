deecbba82245e0a2ef18324786c6d8b1b2d86166
Eldar <elogon@gmail.com>
2020-10-14 13:57:50 -0400

    Implement fully functional logic to generatePassword function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,32 +14,71 @@ function writePassword() {
 function generatePassword() {
   var passwordLength = 0;
   // Promtpt how long password is needed
-  // check is password not less than 8 characters and not more than 128 charachters
+  // check if password not less than 8 characters and not more than 128 charachters
   do {
-
     passwordLength = prompt('How long would you like your password to be? (not less than 8, not more than 128'); 
     if (passwordLength < 8 ||passwordLength > 128) {
       alert('Password length should not be less than 8 or more than 128');
-      
+    }
+  } while (!(passwordLength > 7 && passwordLength < 129));
+  console.log('Password length: ', passwordLength);
+  
+  // Character sets to choose from
+  var lowercaseCharSet = 'abcdefghijklmnopqrstuvwxyz'; // 26
+  var uppercaseCharSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // 26
+  var numericCharSet = '0123456789' // 10
+  var specialCharSet = ' !"#$%&()*+,-./:;<=>?@[\]^_`{|}~' + "'"; // 33
+  
+  var uppercase, lowercase, numeric, special; // varaibles to set choice
+  var password = '';
+  var charsetToUse ='';
+
+  // Prompt what type of charachters to include in generated password
+  // Validate that at least one type of chatacters is selected
+  while (!(uppercase || lowercase || numeric || special)) {
+    // Prompt if include uppercase
+    uppercase = confirm('Would you like to include Uppercase characters in your password?');
+    if (uppercase) {
+      charsetToUse = uppercaseCharSet; // Include uppercase to choose from
     }
 
-  } while (!(passwordLength > 7 && passwordLength < 129));
-
-  console.log('Password length: ', passwordLength);
-
-    // store password length
-  // Prompt what type of charachters to include in generated password
-    // Prompt if include uppercase
     // Prompt if include lowercase
+    lowercase = confirm('Would you like to include Lowercase characters in your password?');
+    if (lowercase) {
+      charsetToUse += lowercaseCharSet; // Include lowercase to choose from
+    }
+    
     //Prompt if include numeric
+    numeric = confirm('Would you like to include Numeric characters in your password?');
+    if (numeric) {
+      charsetToUse += numericCharSet; // Include lowercase to choose from
+    }
+
     //Prompt if include special characters
+    special = confirm('Would you like to include Special characters in your password?');
+    if (special) {
+      charsetToUse += specialCharSet; // Include lowercase to choose from
+    }
 
-  // Validate that at least one type of chatacters is selected
-
+    // Warn if none of the types of characters is selected
+    if (!(uppercase || lowercase || numeric || special)) {
+      alert('At least one type of character should be selected!');
+      charsetToUse = ''; // clear charsetToUse to start over
+    }
+  }
+  
   // Generate password based on criterias
+  console.log('Password length:' + passwordLength + ' Character set: ' + charsetToUse); // For testing: to check if choice is correct
+  // Loop to repeat to generate each character in password
+  for (var i = 1; i <= passwordLength; i++) {
+    positon = Math.floor(Math.random() * (charsetToUse.length));
+    password += charsetToUse.charAt(positon);
+  }
+  
+  console.log(password + '   Password length: ' + password.length); // For testing to confirm password
 
   // Return generated password
-  return 'Password generated';
+  return password;
 }
 
 // Add event listener to generate button
