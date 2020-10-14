fc4431b058f8c0648b584ec0b2d8eedba0951e39
Eldar <elogon@gmail.com>
2020-10-14 09:45:30 -0400

    Describe basic logic using comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,26 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  // Assign generated password to be displayed in passwordText area
   passwordText.value = password;
 
 }
 
 function generatePassword() {
+  // Promtpt how long password is needed
+    // check is password not less than 8 characters and not more than 128 charachters
+    // store password length
+  // Prompt what type of charachters to include in generated password
+    // Prompt if include uppercase
+    // Prompt if include lowercase
+    //Prompt if include numeric
+    //Prompt if include special characters
+
+  // Validate that at least one type of chatacters is selected
+
+  // Generate password based on criterias
+
+  // Return generated password
   return 'Password generated';
 }
 
