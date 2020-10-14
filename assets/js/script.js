8b414aa20e6fdcd6e467fba07d7b7430c98809d2
Eldar <elogon@gmail.com>
2020-10-14 12:05:06 -0400

    Implement password length prompt and check

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,8 +12,21 @@ function writePassword() {
 }
 
 function generatePassword() {
+  var passwordLength = 0;
   // Promtpt how long password is needed
-    // check is password not less than 8 characters and not more than 128 charachters
+  // check is password not less than 8 characters and not more than 128 charachters
+  do {
+
+    passwordLength = prompt('How long would you like your password to be? (not less than 8, not more than 128'); 
+    if (passwordLength < 8 ||passwordLength > 128) {
+      alert('Password length should not be less than 8 or more than 128');
+      
+    }
+
+  } while (!(passwordLength > 7 && passwordLength < 129));
+
+  console.log('Password length: ', passwordLength);
+
     // store password length
   // Prompt what type of charachters to include in generated password
     // Prompt if include uppercase
